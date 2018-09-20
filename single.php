c46ec1ee3df9ef39e8ed0a24dcdada92de2d02e8
Richie Arnold <richard@ambercouch.co.uk>
2018-09-20 09:17:35 +0100

    Add single php and twig templates

---
+++ single.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * The Template for displaying all single posts
+ *
+ * Methods for TimberHelper can be found in the /lib sub-directory
+ *
+ * @package  WordPress
+ * @subpackage  Timber
+ * @since    Timber 0.1
+ */
+
+add_action( 'wp_print_styles', 'ac_enqeue_styles' );
+function ac_enqeue_styles() {
+    //deqeue the current theme files
+    //wp_register_style('ac-style', get_stylesheet_directory() . '/style.css');
+    //wp_enqueue_style( 'ac-style' );
+    wp_deregister_style('parent-style');
+    wp_dequeue_style( 'parent-style' );
+}
+
+$context = Timber::get_context();
+$post = Timber::query_post();
+$context['post'] = $post;
+$context['template'] = $post->post_type;
+$context['postType'] = $post->post_type;
+
+require_once get_theme_file_path() . '/lib/wp-timber/timber--nav-menu.php';
+//require_once get_template_directory() . '/lib/wp-timber/timber--comment-form.php';
+
+if ( post_password_required( $post->ID ) ) {
+	Timber::render( 'single-password.twig', $context );
+} else {
+	Timber::render( array( 'single-' . $post->ID . '.twig', 'single-' . $post->post_type . '.twig', 'single.twig' ), $context );
+}
